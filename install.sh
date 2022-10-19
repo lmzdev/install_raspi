906dda8717ac7053f8c9b98457cb573268e1c889
lmzdev <44636506+lmzdev@users.noreply.github.com>
2022-10-19 19:00:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -52,17 +52,11 @@ echo
 echo "$C>Installing new Packages...$NC"
 sudo apt-get -y -q install dnsutils vim build-essential mc apt-transport-https net-tools traceroute nmap iperf3 toilet linuxlogo highlight htop tty-clock fzf git git-lfs curl wget zsh ca-certificates screen
 
-
-read -p "$C>Install additional Python/Python3 dependencies? [Y/n] $NC" -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    sudo apt-get -y install python3-pip python3-smbus pigpio python-pigpio python3-pigpio python-gpiozero python3-gpiozero python3-rpi.gpio i2c-tools python3-venv
-    python --version
-    python3 --version
-    echo
-    pip3 install virtualenv paho-mqtt RPi.GPIO
-fi
+sudo apt-get -y install python3-smbus pigpio python-pigpio python3-pigpio python-gpiozero python3-gpiozero python3-rpi.gpio RPi.GPIO i2c-tools
+sudo apt-get -y install python3-pip  python3-venv
+python --version
+python3 --version
+ 
 
 read -p "$C>Install Node.js? [Y/n] $NC" -n 1 -r
 echo
@@ -111,6 +105,11 @@ echo
 echo "$C>Shell settings...$NC"
 
 echo "" | sudo tee /etc/motd
+sudo chmod -x /etc/update-motd.d/*
+sudo chmod +x /etc/update-motd.d/98-reboot-required
+sudo chmod +x /etc/update-motd.d/92-unattended-upgrades
+sudo chmod +x /etc/update-motd.d/90-updates-available
+
 curl -sL "https://gist.githubusercontent.com/lmzdev/41f545d9eb93c66d1ef72658ed7026c7/raw/" > ~/.bash_aliases
 curl -sL "https://gist.githubusercontent.com/lmzdev/c03befd8b90a5851c1d96d78904ed39a/raw/" > ~/.bash_prompt
 curl -sL "https://raw.githubusercontent.com/lmzdev/install_raspi/main/.bashrc" > ~/.bashrc
@@ -119,11 +118,12 @@ curl -sL "https://raw.githubusercontent.com/lmzdev/install_raspi/main/.bashrc" >
 sudo wget -q "https://raw.githubusercontent.com/6gk/fet.sh/master/fet.sh" -P "/usr/local/bin"
 sudo chmod 755 "/usr/local/bin/fet.sh"
 
-echo "$C>Raspi-Config settings in non-interactive mode...$NC"
-sudo raspi-config nonint do_i2c 0
-sudo raspi-config nonint do_onewire 0
-sudo raspi-config nonint do_rgpio 0
+#echo "$C>Raspi-Config settings in non-interactive mode...$NC"
+#sudo raspi-config nonint do_i2c 0
+#sudo raspi-config nonint do_onewire 0
+#sudo raspi-config nonint do_rgpio 0
 
+sudo timedatectl set-timezone "Europe/Berlin"
 sudo dpkg-reconfigure locales
 
 echo "$C>Change password (or leave empty): $NC"
